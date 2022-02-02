0b462ac3f0abbc9836d8d175d6e506ab6a2c3224
Alan <alan.szepieniec@gmail.com>
2022-02-02 14:54:17 +0100

    add unsalted merkle trees

--- code/salted_merkle.py
+++ code/salted_merkle.py
@@ -4,7 +4,7 @@ from binascii import hexlify
 import pickle
 
 
-class Merkle:
+class SaltedMerkle:
     def __init__(self, data_array):
         # calculate depth and next power of two
         self.num_leafs = len(data_array)
@@ -20,16 +20,17 @@ class Merkle:
         self.depth -= 1
 
         # append salt to leafs
-        self.leafs = [leaf for leaf in data_array]
+        self.leafs = [(leaf, urandom(24)) for leaf in data_array]
 
         # make room for nodes
         self.nodes = [bytes([0]*32)] * (2 * next_power_of_two)
 
         # populate nodes with hash of leafs
         for i in range(len(self.leafs)):
-            unsalted_bytes = pickle.dumps(self.leafs[i])
+            left_bytes = pickle.dumps(self.leafs[i][0])
+            right_bytes = pickle.dumps(self.leafs[i][1])
             self.nodes[next_power_of_two +
-                       i] = blake2b(unsalted_bytes).digest()
+                       i] = blake2b(left_bytes + right_bytes).digest()
 
         # populate nodes with merger of children, recursively
         i = next_power_of_two
@@ -44,16 +45,18 @@ class Merkle:
         return self.nodes[1]
 
     def open(self, index):
+        salt = self.leafs[index][1]
         authentication_path = []
         index = (1 << self.depth) | index
         while index > 1:
             authentication_path += [self.nodes[index ^ 1]]
             index >>= 1
-        return authentication_path
+        return (salt, authentication_path)
 
     @staticmethod
-    def verify(root, index, path, element):
-        running_hash = blake2b(pickle.dumps(element)).digest()
+    def verify(root, index, salt, path, element):
+        running_hash = blake2b(pickle.dumps(element) +
+                               pickle.dumps(salt)).digest()
         for node in path:
             if index % 2 == 0:
                 running_hash = blake2b(running_hash + node).digest()
--- code/test_merkle.py
+++ code/test_merkle.py
@@ -1,9 +1,10 @@
 from binascii import hexlify
-from merkle import SaltedMerkle
+from salted_merkle import SaltedMerkle
+from merkle import Merkle
 from os import urandom
 
 
-def test_merkle():
+def test_salted_merkle():
     n = 64
     elements = [list(l) for l in zip([urandom(int(urandom(1)[0]))
                                       for i in range(n)], [urandom(int(urandom(1)[0])) for i in range(n)])]
@@ -52,3 +53,48 @@ def test_merkle():
         salt, path = tree.open(i)
         assert(False == SaltedMerkle.verify(
             root, i, urandom(32), path, elements[i]))
+
+
+def test_merkle():
+    n = 64
+    elements = [list(l) for l in zip([urandom(int(urandom(1)[0]))
+                                      for i in range(n)], [urandom(int(urandom(1)[0])) for i in range(n)])]
+
+    tree = Merkle(elements)
+    root = tree.root()
+
+    # opening any leaf should work
+    for i in range(n):
+        path = tree.open(i)
+        assert(Merkle.verify(root, i, path, elements[i]))
+
+    # opening non-leafs should not work
+    for i in range(n):
+        path = tree.open(i)
+        assert(False == Merkle.verify(root, i, path, urandom(51)))
+
+    # opening wrong leafs should not work
+    for i in range(n):
+        path = tree.open(i)
+        j = (i + 1 + (int(urandom(1)[0] % (n-1)))) % n
+        assert(False == Merkle.verify(root, i, path, elements[j]))
+
+    # opening leafs with the wrong index should not work
+    for i in range(n):
+        path = tree.open(i)
+        j = (i + 1 + (int(urandom(1)[0] % (n-1)))) % n
+        assert(False == Merkle.verify(root, j, path, elements[i]))
+
+    # opening leafs to a false root should not work
+    for i in range(n):
+        path = tree.open(i)
+        assert(False == Merkle.verify(
+            urandom(32), i, path, elements[i]))
+
+    # opening leafs with even one falsehood in the path should not work
+    for i in range(n):
+        path = tree.open(i)
+        for j in range(len(path)):
+            fake_path = path[0:j] + [urandom(32)] + path[j+1:]
+            assert(False == Merkle.verify(
+                root, i, fake_path, elements[i]))
